2b8446faab32ac9c1ec1650fa43b0f4ebe5a73bc
MerlinScheurer <merlin.scheurer@Yahoo.de>
2024-05-15 10:19:23 +0200

    Add ta_update to /api/ping call

--- tubearchivist/api/views.py
+++ tubearchivist/api/views.py
@@ -720,6 +720,7 @@ class PingView(ApiBaseView):
             "response": "pong",
             "user": request.user.id,
             "version": ReleaseVersion().get_local_version(),
+            "ta_update": ReleaseVersion().get_update()
         }
         return Response(data)
     
